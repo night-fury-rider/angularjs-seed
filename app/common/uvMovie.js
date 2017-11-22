2860125327a0e9bc250bc3518ebe8380cb1985e7
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-11-22 15:35:15 +0530

    1. Added movie directive. 2. Removed unnecessary white spaces.

---
+++ app/common/uvMovie.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+  uvApp.directive('uvMovie', function() {
+    return {
+      restrict: 'E',    //  E for Element
+      scope: {          //  Isolated Scope.
+        name: '@title'  //  One way data binding. From Controller to Directive
+      },
+      template: '<span> {{ name }} </span>'
+    };
+  });
+
+})();
